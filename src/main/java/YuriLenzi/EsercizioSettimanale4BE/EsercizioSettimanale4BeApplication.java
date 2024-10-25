49c37dd347a3cac8f7e61a0839f887bfc2cd8fea
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-10-25 09:57:43 +0200

    Initial commit

---
+++ src/main/java/YuriLenzi/EsercizioSettimanale4BE/EsercizioSettimanale4BeApplication.java
@@ -0,0 +1,13 @@
+package YuriLenzi.EsercizioSettimanale4BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioSettimanale4BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioSettimanale4BeApplication.class, args);
+	}
+
+}
